71fcc19a2c36db7ae8c99bb6e3b94853ce74dd21
Richard Lan <yiukencw@gmail.com>
2019-06-04 10:54:59 +0100

    Add 6 TV show titles, closes #3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
   return (
     <div className="App">
         <img src={twLogo} className="Movie-Cover" alt="Twilight Zone logo" />
+        <p className="showTitles">
+          <div className="container">
+            <h1 className="item">Black Mirror</h1>
+            <h1 className="item">Breaking Bad</h1>
+            <h1 className="item">Death Note</h1>
+          </div>
+          <div className="container">
+            <h1 className="item">Game of Thrones</h1>
+            <h1 className="item">The Walking Dead</h1>
+            <h1 className="item">The Wire</h1>
+          </div>
+        </p>
     </div>
   );
 }
