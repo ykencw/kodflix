f6b5a957ae4b3c67f5544842453ee9db8d167516
Richard Lan <yiukencw@gmail.com>
2019-06-04 11:15:22 +0100

    Remove Movie Cover, Replace show titles with covers, closes #2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import twLogo from './Twilight_Zone_Logo.jfif';
+import bmLogo from './images/BlackMirror.jpg';
+import bbLogo from './images/BreakingBad.jpg';
+import dnLogo from './images/DeathNote.jpg';
+import gotLogo from './images/GoT.jpg';
+import wdLogo from './images/WalkingDead.jpg';
+import wireLogo from './images/Wire.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={twLogo} className="Movie-Cover" alt="Twilight Zone logo" />
         <p className="showTitles">
           <div className="container">
-            <h1 className="item">Black Mirror</h1>
-            <h1 className="item">Breaking Bad</h1>
-            <h1 className="item">Death Note</h1>
+            <div className="item">
+              <img src={bmLogo} alt="Black Mirror logo" />
+            </div>
+            <div className="item">
+              <img src={bbLogo} alt="Breaking Bad logo" />
+            </div>
+            <div className="item">
+              <img src={dnLogo} alt="Death Note logo" />
+            </div>
           </div>
           <div className="container">
-            <h1 className="item">Game of Thrones</h1>
-            <h1 className="item">The Walking Dead</h1>
-            <h1 className="item">The Wire</h1>
+            <div className="item">
+              <img src={gotLogo} alt="Game of Thrones logo" />
+            </div>
+            <div className="item">
+              <img src={wdLogo} alt="The Walking Dead logo" />
+            </div>
+            <div className="item">
+              <img src={wireLogo} alt="The Wire logo" />
+            </div>
           </div>
         </p>
     </div>
