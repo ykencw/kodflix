0d025f24a40301aef21cc7450322a9329d988532
Richard Lan <yiukencw@gmail.com>
2019-06-04 10:46:30 +0100

    Add Movie Cover, closes #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import twLogo from './Twilight_Zone_Logo.jfif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Welcome to Kodflix</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={twLogo} className="Movie-Cover" alt="Twilight Zone logo" />
     </div>
   );
 }
