6929137b293c3529a126d5c096f96596d6d8a8ac
Richard Lan <yiukencw@gmail.com>
2019-06-04 12:06:10 +0100

    Create Gallery component for further refactorize, closes #9

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import TVOverlay from './TVOverlay'
-import bmLogo from './images/BlackMirror.jpg';
-import bbLogo from './images/BreakingBad.jpg';
-import dnLogo from './images/DeathNote.jpg';
-import gotLogo from './images/GoT.jpg';
-import wdLogo from './images/WalkingDead.jpg';
-import wireLogo from './images/Wire.jpg';
+import Gallery from './Gallery';
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <p className="showTitles">
-          <div className="container">
-            <TVOverlay source={bmLogo} title='Black Mirror' />
-            <TVOverlay source={bbLogo} title='Breaking Bad' />
-            <TVOverlay source={dnLogo} title='Death Note' />
-          </div>
-          <div className="container">
-            <TVOverlay source={gotLogo} title='Game of Thrones' />
-            <TVOverlay source={wdLogo} title='The Walking Dead' />
-            <TVOverlay source={wireLogo} title='The Wire' />
-          </div>
-        </p>
+      <Gallery />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import TVOverlay from './TVOverlay';
+import bmLogo from './images/BlackMirror.jpg';
+import bbLogo from './images/BreakingBad.jpg';
+import dnLogo from './images/DeathNote.jpg';
+import gotLogo from './images/GoT.jpg';
+import wdLogo from './images/WalkingDead.jpg';
+import wireLogo from './images/Wire.jpg';
+
+function Gallery() {
+    return (
+      <p className="showTitles">
+        <div className="container">
+          <TVOverlay source={bmLogo} title='Black Mirror' />
+          <TVOverlay source={bbLogo} title='Breaking Bad' />
+          <TVOverlay source={dnLogo} title='Death Note' />
+        </div>
+        <div className="container">
+          <TVOverlay source={gotLogo} title='Game of Thrones' />
+          <TVOverlay source={wdLogo} title='The Walking Dead' />
+          <TVOverlay source={wireLogo} title='The Wire' />
+        </div>
+      </p>
+    );
+  }
+
+export default Gallery;
