07d027ec53f935c61ef242721d9685474b1f99b0
Richard Lan <yiukencw@gmail.com>
2019-06-11 14:12:59 +0100

    Update setup to run both backend and frontend in parallel, closes #22

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const getTVSeries = require('./tvseries');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/rest/tvseries', (req, res) => res.send(getTVSeries()));
 
