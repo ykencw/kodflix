95231daf1aa15b871acd8fd55808e1cb1e9b6513
Richard Lan <yiukencw@gmail.com>
2019-06-11 15:47:49 +0100

    Offer up tvseries at /rest/tvseries

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 3001;
 const buildPath = '../../build';
 
+app.get('/rest/tvseries', (_req, res) => res.send(getTVSeries()));
+
 app.use(express.static(path.join(__dirname, buildPath)));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, buildPath, 'index.html'));
 });
-
-app.get('/rest/tvseries', (req, res) => res.send(getTVSeries()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
