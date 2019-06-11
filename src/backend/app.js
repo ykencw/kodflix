c462f01022a9533ee0fb33a4e0c3cdc7333679c9
Richard Lan <yiukencw@gmail.com>
2019-06-11 15:34:44 +0100

    Enable the backend server in production, closes #25

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const getTVSeries = require('./tvseries');
+const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+const buildPath = '../../build';
+
+app.use(express.static(path.join(__dirname, buildPath)));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, buildPath, 'index.html'));
+});
 
 app.get('/rest/tvseries', (req, res) => res.send(getTVSeries()));
 
