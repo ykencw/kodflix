f9f34923084c6718fe4f8606367c97fd01186bb6
Richard Lan <yiukencw@gmail.com>
2019-06-14 15:27:30 +0100

    Create the vertical menu panel, closes #37

--- src/frontend/Menu.js
+++ src/frontend/Menu.js
@@ -4,12 +4,37 @@ import React from 'react';
 import './Menu.css';
 
 class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+
     handleClick = () => {
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
+        }));
         console.log('The menu works!')
     }
 
     render() {
-        return (<button className='Menu' onClick={this.handleClick}><img className='icon' src={menuicon} alt='menu icon'></img></button>);
+        return (
+        <div className={`Menu ${this.state.isOpen ? 'is-visible' : ''}`}>
+            <button onClick={this.handleClick}>
+                <img className='icon' src={menuicon} alt='menu icon'></img>
+            </button>
+            <div className='panel'>
+                <div className='dimmer' onClick={this.handleClick} />
+                <div className='bar'><div className='title'>Hello menu!</div>
+                    <div className='container'>
+                        <div className='item'>Faux Option 1</div>
+                        <div className='item'>Faux Option 2</div>
+                        <div className='item'>Faux Option 3</div>
+                    </div>
+                </div>
+            </div>
+        </div>);
     }
 }
 
