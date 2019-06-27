7ff615d82388226dd1be65d80826beeebabd5677
Richard Lan <yiukencw@gmail.com>
2019-06-27 20:05:34 +0100

    Switch resave to false

--- src/backend/db.js
+++ src/backend/db.js
@@ -27,7 +27,7 @@ function sessionStore() {
     return session({
         secret: DB_SESSION_SALT,
         cookie: { maxAge: 10 * 60 * 1000 },
-        resave: true,
+        resave: false,
         rolling: true, // Updates client session id so they don't expire early
         saveUninitialized: false, // Save space by ignoring uninit'd cookies
         unset: 'destroy',
